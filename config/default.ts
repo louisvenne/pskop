8c8617b14c493eddb0fe858cbc3a42fa0031baa1
Louis VENNE <louis.venne@protonmail.com>
2020-10-31 13:07:09 +0100

    Add app default config file

---
+++ config/default.ts
@@ -0,0 +1,14 @@
+/**
+ * Powered by @louisvenne
+ */
+
+
+const config = {
+  database: {
+    dialect: 'sqlite',
+    storage: 'data/database.sqlite',
+    logging: false,
+  }
+};
+
+export default config;
